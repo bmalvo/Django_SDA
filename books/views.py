d15380cb11dc0058fbd782a32e9be36124dd883f
patryk_s <boydmalvo@gmail.com>
2022-04-10 00:47:40 +0200

    uuids-a added and uuids_b added

--- books/views.py
+++ books/views.py
@@ -1,3 +1,21 @@
+from uuid import uuid4
+
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def get_hello(request: WSGIRequest) -> HttpResponse:
+    return HttpResponse('<h1>Hello World<h1>')
+
+
+def get_uuids_a(request: WSGIRequest) -> HttpResponse:
+    uuids = [f'{uuid4()}' for _ in range(10)]
+    return HttpResponse(f'uuids={uuids}')
+
+def get_uuids_b(request: WSGIRequest) -> JsonResponse:
+    uuids = [f'{uuid4()}' for _ in range(10)]
+    return JsonResponse({'uuids':uuids})
--- library/urls.py
+++ library/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from books.views import get_hello, get_uuids_a, get_uuids_b
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', get_hello),
+    path('uuids-a', get_uuids_a),
+    path('uuids-b', get_uuids_b),
+     ]
