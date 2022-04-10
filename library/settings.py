4dd62ad4a5750cd7453a305663f041879f2c7e5a
patryk_s <boydmalvo@gmail.com>
2022-04-10 12:29:54 +0200

    excersise FBV

--- library/settings.py
+++ library/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x%v*ol=npc%zr+b7&_9msczoqw2yy)2$w&fnohugb=5q0mqn)e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
