461908b4208d033bda259c9a731fda66ce76275d
patryk_s <boydmalvo@gmail.com>
2022-04-10 13:27:32 +0200

    ...

--- books/urls.py
+++ books/urls.py
@@ -1,15 +1,11 @@
 
-from django.conf import settings
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from books.views import get_hello, get_uuids_a, get_uuids_b, get_argument_from_path, get_arguments_from_query, \
     check_http_query_type, get_headers, raise_error_for_fun
 
 urlpatterns = [
-
     path('', get_hello),
-    path('books/', include('books.urls')),
     path('uuids-a', get_uuids_a),
     path('uuids-b', get_uuids_b),
     path('path-args/<int:x>/<str:y>/<slug:z>', get_argument_from_path, name='get_from_path'),
@@ -19,5 +15,6 @@ urlpatterns = [
     path('raise-error', raise_error_for_fun, name='raise_error'),
      ]
 
-if settings.DEBUG:
-    urlpatterns.append(path('admin/', admin.site.urls))
+
+
+
--- library/settings.py
+++ library/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x%v*ol=npc%zr+b7&_9msczoqw2yy)2$w&fnohugb=5q0mqn)e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
