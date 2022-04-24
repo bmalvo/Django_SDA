7ca63a5200daf07e99c447144023154701751c85
patryk_s <boydmalvo@gmail.com>
2022-04-24 12:26:16 +0200

    add book detail

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,8 @@
 from django.urls import path
 
 from books.views import get_hello, get_uuids_a, get_uuids_b, get_argument_from_path, get_arguments_from_query, \
-    check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, CategoryListTemplateView, BooksListView
+    check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, CategoryListTemplateView, \
+    BooksListView, BookDetailsView
 
 urlpatterns = [
     path('', get_hello),
@@ -16,6 +17,7 @@ urlpatterns = [
     path('author_list', AuthorListBaseView.as_view(), name='author_list'),
     path('category_list', CategoryListTemplateView.as_view(), name='author_list'),
     path('books_list', BooksListView.as_view(), name='books_list'),
+    path('book_detail/<int:pk>/', BookDetailsView.as_view(), name='books_detail'),
 
      ]
 
--- books/views.py
+++ books/views.py
@@ -3,9 +3,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import BadRequest
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from books.models import BookAuthor, Category, Book
 
@@ -28,6 +28,14 @@ class BooksListView(ListView):
     template_name = 'books_list.html'
     model = Book
     paginate_by = 10
+
+
+class BookDetailsView(DetailView):
+    template_name = 'book_detail.html'
+    model = Book
+
+    def get_object(self, **kwargs):
+        return get_object_or_404(Book, id=self.kwargs.get("pk"))
 
 
 def get_hello(request: WSGIRequest) -> HttpResponse:
