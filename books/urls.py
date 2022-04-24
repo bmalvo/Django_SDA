b8b2ef68c48ae1f5195cff35118291ed3e5de61e
patryk_s <boydmalvo@gmail.com>
2022-04-24 10:34:13 +0200

    First books authors view

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 
 from books.views import get_hello, get_uuids_a, get_uuids_b, get_argument_from_path, get_arguments_from_query, \
-    check_http_query_type, get_headers, raise_error_for_fun
+    check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView
 
 urlpatterns = [
     path('', get_hello),
@@ -13,6 +13,8 @@ urlpatterns = [
     path('query-type', check_http_query_type, name='check_query_type'),
     path('get-headers', get_headers, name='get_headers'),
     path('raise-error', raise_error_for_fun, name='raise_error'),
+    path('author_list', AuthorListBaseView.as_view(), name='author_list'),
+
      ]
 
 
--- books/views.py
+++ books/views.py
@@ -1,13 +1,22 @@
 from uuid import uuid4
-
+from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import BadRequest
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views import View
+
+from books.models import BookAuthor
 
 
-# Create your views here.
-from django.views.decorators.csrf import csrf_exempt
+class AuthorListBaseView(View):
+    template_name = 'author_list.html'
+    queryset = BookAuthor.objects.all()  # type: ignore
+
+    def get(self, request: WSGIRequest, *args, **kwargs):
+        contex = {'authors': self.queryset}
+        return render(request, template_name=self.template_name, context=contex)
+
 
 
 def get_hello(request: WSGIRequest) -> HttpResponse:
