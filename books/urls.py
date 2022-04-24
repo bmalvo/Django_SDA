e7c503482ae3923c4e79b6934cb640ea963fd206
patryk_s <boydmalvo@gmail.com>
2022-04-24 12:07:22 +0200

    Book list pagination

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 
 from books.views import get_hello, get_uuids_a, get_uuids_b, get_argument_from_path, get_arguments_from_query, \
-    check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, CategoryListTemplateView
+    check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, CategoryListTemplateView, BooksListView
 
 urlpatterns = [
     path('', get_hello),
@@ -15,6 +15,7 @@ urlpatterns = [
     path('raise-error', raise_error_for_fun, name='raise_error'),
     path('author_list', AuthorListBaseView.as_view(), name='author_list'),
     path('category_list', CategoryListTemplateView.as_view(), name='author_list'),
+    path('books_list', BooksListView.as_view(), name='books_list'),
 
      ]
 
--- books/views.py
+++ books/views.py
@@ -5,9 +5,9 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
-from books.models import BookAuthor, Category
+from books.models import BookAuthor, Category, Book
 
 
 class AuthorListBaseView(View):
@@ -23,6 +23,11 @@ class CategoryListTemplateView(TemplateView):
     template_name ='category_list.html'
     extra_context = {'categories': Category.objects.all()}  # type: ignore
 
+
+class BooksListView(ListView):
+    template_name = 'books_list.html'
+    model = Book
+    paginate_by = 10
 
 
 def get_hello(request: WSGIRequest) -> HttpResponse:
